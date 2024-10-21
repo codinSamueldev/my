d370f0f3632b130e1cf8e23865dd694c42c9d053
Samuel Henao <codinsamueldev@gmail.com>
2024-10-20 22:34:40 -0500

    add configuration to serve static files properly

--- my/settings.py
+++ my/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -118,14 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-"""
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     "var/www/static/",
 ]
-"""
+
 
 
 # User-uploaded files.
--- my/urls.py
+++ my/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
