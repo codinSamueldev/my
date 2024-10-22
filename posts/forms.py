ae5234086a70b8d00bae6433caf3c99cdfbb908a
Samuel Henao <codinsamueldev@gmail.com>
2024-10-22 18:17:18 -0500

    change default widgets of the form

--- posts/forms.py
+++ posts/forms.py
@@ -3,6 +3,11 @@ from .models import PostModel
 
 
 class PostForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["content"].widget.attrs.update(placeholder="What's on ya mind?")
+        self.fields["picture"].required = False
+
     class Meta:
         model = PostModel
         fields = ["content", "picture"]
