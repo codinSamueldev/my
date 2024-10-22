84f144fd66e397f73366ba515cbcafa6f801ec20
Samuel Henao <codinsamueldev@gmail.com>
2024-10-21 23:20:21 -0500

    create class-based view to render posts and its form

--- my/views.py
+++ my/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+
+from posts.forms import PostForm
+from posts.models import PostModel
+
+class PostFormView(FormView):
+    template_name = "index.html"
+    form_class = PostForm
+    success_url = "/"
 
 
-def home(request):
-    # posts = Posts.objects.all()
-    posts = "Nothing yet .-."
-    
-    return render(request, 'index.html', {"posts": posts})
+    def get(self, request):
+        posts = PostModel.objects.all()
+
+        form = PostForm()
+        return render(request, self.template_name, {"posts": posts, "form": form})
+
+
+    def form_valid(self, form):
+        form.save()
+
+        return super().form_valid(form)
 
