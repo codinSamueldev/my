619551b9ca30129baa5bbdb4660a9fc6acdbf029
Samuel Henao <codinsamueldev@gmail.com>
2024-10-29 00:00:34 -0500

    fix: superusers creation methods

--- user/models.py
+++ user/models.py
@@ -4,20 +4,23 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
+
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
+        
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(username, email, password, **extra_fields)
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password=password, **extra_fields)
+        
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 class UserModel(AbstractBaseUser):
     username = models.CharField(max_length=50, unique=True)
@@ -31,8 +34,7 @@ class UserModel(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     
     # Django's built-in fields for admin and permissions
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
@@ -41,5 +43,20 @@ class UserModel(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+    
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
 
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
 
