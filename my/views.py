15f59f92308a1765d4baf618d1e54db5f8a4aaba
Samuel Henao <codinsamueldev@gmail.com>
2024-10-31 16:55:13 -0500

    new: add comments component and its logic to handle them

--- my/views.py
+++ my/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
+from django.http import HttpResponse
 
 from posts.forms import PostForm, PostCommentsForm
 from posts.models import PostModel, PostCommentsModel
@@ -11,23 +12,47 @@ class PostFormView(FormView):
 
 
     def get(self, request):
-        posts = PostModel.objects.all()
+        posts = PostModel.objects.all().prefetch_related('postcommentsmodel_set')
+        comments = PostCommentsModel.objects.all()
 
-        form = PostForm()
-        return render(request, self.template_name, {"posts": posts, "form": form})
+        comment_form = PostCommentsForm()
+        post_form = PostForm()
 
+        return render(request, self.template_name, {'posts': posts, 'form': post_form, 'comments': comments, 'comment_form': comment_form})
+    
 
-    def form_valid(self, form):
-        post = form.save(commit=False)
-        post.posted_by = self.request.user # Associate the post with the current user.
-        post.save()
+    def post(self, request, *args, **kwargs):
+        comment_form = PostCommentsForm(request.POST, request.FILES)
+        post_form = PostForm(request.POST, request.FILES)
 
-        return super().form_valid(form)
+        if 'post' in request.POST:
+            if post_form.is_valid():
+                post = post_form.save(commit=False)
+                post.posted_by = self.request.user # Associate the post with the current user.
+                post.save()
+                return redirect("home")
 
-"""
-class PostCommentsView(FormView):
-    template_name = "index.html"
-    form_class = PostCommentsForm
-    success_url = "/" 
-"""
+        if 'submit_comment' in request.POST:
+            post_id = request.POST.get("post")
+            
+            print(f"\n  {post_id}  \n")
+
+            if post_id:
+                commented_post = PostModel.objects.get(id=post_id)
+            else:
+                return HttpResponse("Aqui no hay ningun post_id pelao")
+
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.commented_by = request.user
+                comment.post = commented_post
+                comment.save()
+                return redirect("home")
+        
+        
+        posts = PostModel.objects.all().prefetch_related('postcommentsmodel_set')
+        comments = PostCommentsModel.objects.all()
+
+        return render(request, self.template_name, {'posts': posts, 'form': post_form, 'comments': comments, 'comment_form': comment_form})
+
 
